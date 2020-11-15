71ae4283f1f10dafa7442a55b248ca3ca35e7c1b
kadir <kadir_irpik.06@hotmail.com>
2020-11-15 20:22:06 +0300

    İlk Gönderim

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 module.exports.index=function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Endüstri Meslek' });
 };
 
 
