c959078e66ac41b14de9b98b2859750bd65cac82
kadir <kadir_irpik.06@hotmail.com>
2020-12-25 11:45:10 +0300

    DB Son

--- app_api/controllers/mekanlar.js
+++ app_api/controllers/mekanlar.js
@@ -7,13 +7,78 @@ const cevapOlustur=function(res,status,content){
 
 }
 
-const mekanlariListele=function(req, res) {
-	cevapOlustur(res,200,{"durum":"basarili"});
+const mekanlariListele=async(req, res) =>{
+	//URL'den enlem ve boylam parametrelerini al
+	var boylam= parseFloat(req.query.boylam);
+	var enlem = parseFloat(req.query.enlem);
+	//alınan bilgilerden nokta tanımla
+	var nokta = {
+		type: "Point",
+		coordinates: [enlem,boylam]
+	};//coğrafi seçenekleri ekle
+	var geoOptions = {
+		distanceField: "mesafe",
+		spherical: true,
+		key:"koordinatlar"
+	};
+	if (!enlem || !boylam) {
+		cevapOlustur(res, 404, {
+			"mesaj": "enlem ve boylam zorunlu parametreler"
+		});
+		return;
+	}
+		try {
+			const sonuc= await Mekan.aggregate([
+				{
+					$geoNear: {
+						near:nokta,
+						...geoOptions
+
+					}
+				}
+			]);
+			const mekanlar= sonuc.map(mekan=> { return {
+				_id: mekan._id,
+				ad: mekan.ad,
+				adres: mekan.adres,
+				puan: mekan.puan,
+				imkanlar: mekan.imkanlar,
+				mesafe: mekan.mesafe.toFixed()+'m',
+			} });
+			cevapOlustur (res, 200, mekanlar);
+		}
+		catch(e){
+			console.log(e);
+			cevapOlustur (res, 404, e);
+		}
 }
 
-const mekanEkle=function(req, res) {
-	cevapOlustur(res,200,{"durum":"basarili"});
-}
+const mekanEkle= function (req, res) {
+	 	Mekan.create({
+			ad: req.body.ad,
+			adres: req.body.adres,
+			imkanlar: req.body.imkanlar.split(","),
+			koordinatlar: [parseFloat(req.body.enlem),parseFloat(req.body.boylam)],
+			saatler: [
+			{
+				gunler: req.body.gunler1,
+				acilis: req.body.acilis1,
+				kapanis: req.body.kapanis1,
+				kapali: req.body.kapali1
+			}, {
+				gunler: req.body.gunler2,
+				acilis: req.body.acilis2,
+				kapanis: req.body.kapanis2,
+				kapali: req.body.kapali2
+			}]
+		 }, function(hata, mekan) {
+			 if(hata) {
+				 cevapOlustur (res, 400, hata);
+			 }else {
+				 cevapOlustur (res, 201, mekan);
+			 }
+		});
+};
 
 const mekanGetir=function(req, res) {
 	if(req.params&&req.params.mekanid){
@@ -34,13 +99,67 @@ const mekanGetir=function(req, res) {
 		cevapOlustur(res,404,{"durum":"istekte mekanid yok!"})
 }
 
-const mekanGuncelle=function(req, res) {
-	cevapOlustur(res,200,{"durum":"basarili"});
+const mekanGuncelle= function(req, res) {
+ if (!req.params.mekanid) {
+	 cevapOlustur(res, 404, {"mesaj": "Bulunamadı. mekanid gerekli"});
+	 return;
+ }
+	 Mekan
+	 .findById(req.params.mekanid)
+	 //- işareti yorumlar ve puan dışında herşeyi almamızı söyler
+	 .select('-yorumlar -puan')
+	 .exec(
+		 function(hata, gelenMekan) {
+			 if (!gelenMekan) {
+				cevapOlustur(res, 404, {"mesaj": "mekanid bulunamadı"});
+				return;
+			 }else if (hata) {
+				cevapOlustur(res, 400, hata);
+				return;
+			 }
+				gelenMekan.ad = req.body.ad;
+				gelenMekan.adres = req.body.adres;
+				gelenMekan.imkanlar = req.body.imkanlar.split(",");
+				gelenMekan.koordinatlar = [parseFloat(req.body.enlem),parseFloat(req.body.boylam)];
+				gelenMekan.saatler = [{
+					gunler: req.body.gunler1,
+					acilis: req.body.acilis1,
+					kapanis: req.body.kapanis1,
+					kapali: req.body.kapali1,
+				},{
+					gunler: req.body.gunler2,
+					acilis: req.body.acilis2,
+					kapanis: req.body.kapanis2,
+					kapali: req.body.kapali2,
+				}];
+				gelenMekan.save(function(hata, mekan){
+					if (hata) {
+						cevapOlustur(res, 404,hata);
+					}else {
+						cevapOlustur(res, 200, mekan);
+					}
+				});
+		 });
 }
 
-const mekanSil=function(req, res) {
-	cevapOlustur(res,200,{"durum":"basarili"});
-}
+const mekanSil= function(req, res) {
+			var mekanid = req.params.mekanid;
+			if (mekanid) {
+				Mekan
+				.findByIdAndRemove(mekanid)
+				.exec(
+					function(hata, gelenMekan) {
+						if (hata) {
+							cevapOlustur(res, 404, hata);
+							return;
+						}
+							cevapOlustur(res, 204, null);
+					}
+				);
+			}else {
+				cevapOlustur(res, 404, {"mesaj": "mekanid bulunamadı"});
+			}
+};
 
 module.exports={
 	mekanlariListele,
